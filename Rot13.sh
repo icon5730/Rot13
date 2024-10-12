4be5ed15ad92ce8a60e02584767344e4a21665a1
icon5730 <166230648+icon5730@users.noreply.github.com>
2024-10-12 21:29:37 +0300

    Add files via upload

--- Rot13.sh
+++ Rot13.sh
@@ -27,8 +27,8 @@ fi
 }
 
 function process(){
-read -p "$(echo -e "\n$cyan[?]$endcolor$yellow Please specify wheter you'd like to [$endcolor${red}E$endcolor$yellow]ncrypt or [$endcolor${green}D$endcolor$yellow]ecrypt the file contents: $endcolor")" file
-read -p "$(echo -e "$cyan[?]$endcolor$yellow Please provide a name for the output file: $endcolor")" name
+read -p "$(echo -e "\n$cyan[?]$endcolor$yellow Please specify wheter you'd like to$endcolor$red [E]ncrypt$endcolor$yellow or $green[D]ecrypt$endcolor$yellow the file contents: $endcolor")" file
+read -p "$(echo -e "\n$cyan[?]$endcolor$yellow Please provide a name for the output file: $endcolor")" name
 if [ -z $name ]
 	then
 	echo -e "$red[!] Name was not specified. Please specify a name" ; sleep 0.3
@@ -38,7 +38,13 @@ if [ -z $name ]
 fi
 case $file in
 E)
-echo -e "$cyan[*]$endcolor$red File encryption was selected$endcolor"
+echo -e "$cyan[*]$endcolor$red File encryption was selected$endcolor\n"
+echo -e "$cyan[+]$endcolor$blue Encrypting the file's contents as $name.encrypted.txt...$endcolor" ; sleep 0.3
+cat $path | tr 'A-Za-z' 'N-ZA-Mn-za-m' > $name.encrypted.txt
+echo -e "\n$cyan[*]$endcolor$yellow The file was encrypted with Rot13 as $endcolor$green$name.encrypted.txt$endcolor"
+;;
+e)
+echo -e  "$cyan[*]$endcolor$red File encryption was selected$endcolor\n"
 echo -e "$cyan[+]$endcolor$blue Encrypting the file's contents as $name.encrypted.txt...$endcolor" ; sleep 0.3
 cat $path | tr 'A-Za-z' 'N-ZA-Mn-za-m' > $name.encrypted.txt
 echo -e "\n$cyan[*]$endcolor$yellow The file was encrypted with Rot13 as $endcolor$green$name.encrypted.txt$endcolor"
@@ -49,21 +55,33 @@ echo -e "$cyan[+]$endcolor$blue Decrypting the file's contents as $name.decrypte
 cat $path | tr 'N-ZA-Mn-za-m' 'A-Za-z' > $name.decrypted.txt
 echo -e "\n$cyan[*]$endcolor$yellow The file was decrypted with Rot13 as $endcolor$green$name.decrypted.txt$endcolor"
 ;;
+d)
+echo -e "$cyan[*]$endcolor$green File decryption was selected$endcolor\n"
+echo -e "$cyan[+]$endcolor$blue Decrypting the file's contents as $name.decrypted.txt...$endcolor" ; sleep 0.3
+cat $path | tr 'N-ZA-Mn-za-m' 'A-Za-z' > $name.decrypted.txt
+echo -e "\n$cyan[*]$endcolor$yellow The file was decrypted with Rot13 as $endcolor$green$name.decrypted.txt$endcolor"
+;;
 *)
 echo -e "$red[!] Invalid encryption/decryption input!$endcolor" ; sleep 0.3
 process
 ;;
 esac
-read -p "$(echo -e "$cyan[?]$endcolor$yellow Would you like to encrypt/decyrpt another file? [Y/N] $endcolor")" choice
+read -p "$(echo -e "\n$cyan[?]$endcolor$yellow Would you like to encrypt/decyrpt another file? [Y/N] $endcolor")" choice
 case $choice in
 Y)
+pathtest
+;;
+y)
 pathtest
 ;;
 N)
 echo -e "$green[*] Exiting...$endcolor" ; sleep 0.3
 exit 1
 ;;
-
+n)
+echo -e "$green[*] Exiting...$endcolor" ; sleep 0.3
+exit 1
+;;
 *)
 echo -e "$red[!] Invalid input. Exiting...$endcolor" ; sleep 0.3
 exit 1
